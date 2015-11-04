172c0fb8b401d6fe31294d59aba4452c461dd544
Dimitri75 <bdimitri75@gmail.com>
2015-11-04 14:15:05 -0500

    Fix eventHandler

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -24,12 +24,12 @@ public class Controller {
     @FXML
     private AnchorPane anchorPane;
 
-    private static Integer PACE = 20;
-    private static Integer NB_PIGEONS = 50;
+    private final static Integer PACE = 10;
+    private final static Integer NB_PIGEONS = 10;
 
     private Boolean started = false;
     private Graph graph;
-    private CircularQueue<Food> foodCircularQueue = new CircularQueue(5);   // TODO : Ajouter un listener sur la liste pour notifier les pigeons de chaque modification
+    private CircularQueue<Food> foodCircularQueue = new CircularQueue(3);   // TODO : Ajouter un listener sur la liste pour notifier les pigeons de chaque modification
     private List<Character> pigeonsList = new ArrayList<>();  // TODO : A l'ajout de nourriture, démarrer le trajet des pigeons
     private Character child;
 
@@ -41,29 +41,31 @@ public class Controller {
         child = new Character(0, 0, Image.CHILD);
         anchorPane.getChildren().add(child.getShape());
 
-        anchorPane.getScene().addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            double x = child.getShape().getX();
-            double y = child.getShape().getY();
+        if (!started) {
+            anchorPane.getScene().addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+                double x = child.getShape().getX();
+                double y = child.getShape().getY();
 
-            switch (event.getCode()){
-                case UP:
-                    if (y - PACE >= 0)
-                        child.getShape().setY(y - PACE);
-                    break;
-                case DOWN:
-                    if (y + PACE + child.getShape().getHeight() <= anchorPane.getPrefHeight())
-                        child.getShape().setY(y + PACE);
-                    break;
-                case LEFT:
-                    if (x - PACE >= 0)
-                        child.getShape().setX(x - PACE);
-                    break;
-                case RIGHT:
-                    if (x + PACE + child.getShape().getWidth() <= anchorPane.getPrefWidth())
-                        child.getShape().setX(x + PACE);
-                    break;
-            }
-        });
+                switch (event.getCode()) {
+                    case UP:
+                        if (y - PACE >= 0)
+                            child.getShape().setY(y - PACE);
+                        break;
+                    case DOWN:
+                        if (y + PACE + child.getShape().getHeight() <= anchorPane.getPrefHeight())
+                            child.getShape().setY(y + PACE);
+                        break;
+                    case LEFT:
+                        if (x - PACE >= 0)
+                            child.getShape().setX(x - PACE);
+                        break;
+                    case RIGHT:
+                        if (x + PACE + child.getShape().getWidth() <= anchorPane.getPrefWidth())
+                            child.getShape().setX(x + PACE);
+                        break;
+                }
+            });
+        }
     }
 
     public void initPigeons(){
@@ -80,8 +82,8 @@ public class Controller {
                     randX = random.nextInt((int) anchorPane.getWidth());
                     randY = random.nextInt((int) anchorPane.getHeight());
 
-                    randX -= randX % PACE;
-                    randY -= randY % PACE;
+                    randX -= randX % PACE - 2*PACE;
+                    randY -= randY % PACE - 2*PACE;
                 }
                 character.getShape().setX(randX);
                 character.getShape().setY(randY);
@@ -140,8 +142,8 @@ public class Controller {
                 foodCircularQueue.peek().setFoodState(FoodState.BAD);
             }
 
-            int x = ((int) e.getSceneX()) - (((int) e.getSceneX()) % PACE);
-            int y = ((int) e.getSceneY()) - (((int) e.getSceneY()) % PACE);
+            int x = ((int) e.getSceneX()) - (((int) e.getSceneX()) % PACE) - 2*PACE;
+            int y = ((int) e.getSceneY()) - (((int) e.getSceneY()) % PACE) - 2*PACE;
 
             Food food = new Food(x, y);
             anchorPane.getChildren().add(food.getShape());
